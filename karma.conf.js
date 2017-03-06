218804bc53ae52c868f73e2e03fbf74633cb5e48
Dmitriy Dubson <ddubson@pivotal.io>
2017-03-06 15:11:22 -0500

    Add PhantomJS as headless browser alternative

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ var webpackConfig = require('./webpack.config')
 
 module.exports = function (config) {
     config.set({
-        browsers: ['Chrome'],
+        browsers: ['Chrome', 'PhantomJS'],
         singleRun: true,
         frameworks: ['jasmine'],
         files: [
